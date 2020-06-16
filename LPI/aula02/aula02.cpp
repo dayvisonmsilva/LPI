3b8c85d971cba47978d9a69b16e61ad2e49589e7
dayvisongameplays <dayvisoneryc@gmail.com>
2020-06-16 10:30:47 -0300

    atividade aula 02

--- LPI/aula02/aula02.cpp
+++ LPI/aula02/aula02.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <string>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
-    return 0;
+  if (std::string(argv[1]) == "add") {
+    if (argc == 2){
+      std::string mensagem;
+       
+      std::cout << "Digite uma mensagem: ";
+      std::getline(std::cin, mensagem);
+      std::cout << mensagem;
+    }
+
+    if (argc == 3) {
+      std::cout << "Mensagem adicionada: " << argv[2];
+    }
+
+  } else {
+
+    if (argc <= 1){
+      std::cout << "Uso: " << argv[0] << " add <mensagem>";
+      return -1;
+    }
+
+  } 
+
+  return 0;
 }
