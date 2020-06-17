f465080598dcbdbe1f6953df8ed938357b2edc2e
dayvisongameplays <dayvisoneryc@gmail.com>
2020-06-17 09:38:48 -0300

    atividade aula 03

--- LPI/aula02/aula02.cpp
+++ LPI/aula02/aula02.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 int main(int argc, char *argv[]) {
+  std::ofstream arquivo_saida{"mensagem.txt", std::ios::app};
 
   if (argc <= 1) {
     std::cout << "Uso: " << argv[0] << " add <mensagem>";
@@ -12,15 +14,18 @@ int main(int argc, char *argv[]) {
     if (std::string(argv[1]) == "add")  {
       std::string mensagem;
        
-      std::cout << "Digite uma mensagem: ";
+      std::cout << "Digite uma mensagem: " << std::endl;
       std::getline(std::cin, mensagem);
-      std::cout << mensagem << std::endl;
+      std::cout << "Mensagem adicionada: " << mensagem << std::endl;
+
+      arquivo_saida << message << std::endl;
     }
   }
 
   if (argc == 3) {
     if (std::string(argv[1]) == "add")  {
       std::cout << "Mensagem adicionada: " << argv[2];
+      arquivo_saida << argv[2] << std::endl;
     }
   }
   
