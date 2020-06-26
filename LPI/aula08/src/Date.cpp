6d334d09d22ec9e2ed6a59987d4938c21ff296ec
dayvisongameplays <dayvisoneryc@gmail.com>
2020-06-26 02:10:37 -0300

    atividade aula 08

---
+++ LPI/aula08/src/Date.cpp
@@ -0,0 +1,19 @@
+#include "../include/Date.h"
+
+#include <iostream>
+#include <sstream>
+
+Date::Date() : year(0), month(0), day(0)
+{
+}
+
+void Date::set_from_string(const std::string& date) {
+    std::stringstream stream(date);
+    char discard;
+
+    stream >> day;
+    stream >> discard; 
+    stream >> month;
+    stream >> discard;
+    stream >> year;
+}
---
+++ LPI/aula08/src/test.cpp
@@ -0,0 +1,27 @@
+#include "../include/Date.h"
+#include "../include/Diary.h"
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    std::string date = "25/06/2020";
+    Date d;
+
+    std::stringstream stream(date);
+    char discard;
+
+    stream >> d.day;
+    stream >> discard; //
+    stream >> d.month;
+    stream >> discard;
+    stream >> d.year;
+
+    std::cout << d.day << std::endl;
+    std::cout << d.month << std::endl;
+    std::cout << d.year << std::endl;
+
+    return 0;
+}
